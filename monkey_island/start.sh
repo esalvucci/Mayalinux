a077539b70b27b19b00475372d084b060d212e5e
Enrico Salvucci <esalvuz@hotmail.it>
2012-04-03 19:00:15 +0200

    monkey_island/start.sh

--- monkey_island/start.sh
+++ monkey_island/start.sh
@@ -3,8 +3,8 @@
 export HOME='/home/'$USER'/Documenti/matrix/'
 export PS1='\e[0;32m \[\e]0;Matrix\a\]Matrix $ '
 #export PS1="\e[0;32m[\u@\h \W]\$ \e[m "
-rm -rf ./matrix
+rm -rf ./monkey_island
 
-cp -r ./.matrix/ ./matrix/
-cd ./matrix/
+cp -r ./.monkey_island/ ./monkey_island/
+cd ./monkey_island/
 source azione.sh
