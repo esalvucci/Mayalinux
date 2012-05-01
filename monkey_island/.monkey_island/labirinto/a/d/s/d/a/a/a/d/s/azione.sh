745c51a61aa8c51094d4e046311cf93317d50a7a
Enrico Salvucci <esalvuz@hotmail.it>
2012-05-01 18:06:48 +0200

    aggiornato azione.sh del labirinto

--- monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-if [ -e $monkey_island_dir/nave/seppia.sh ]; then rm $monkey_island_dir/nave/seppia.sh > /dev/null 
+if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/pollo ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/pollo > /dev/null 
 fi
 
-if [ -e $monkey_island_dir/nave/diversiv.sh ]; then rm $monkey_island_dir/nave/diversiv.sh > /dev/null 
+if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/vanga ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/vanga > /dev/null 
 fi
 
-if [ -e $monkey_island_dir/nave/sabotagg.sh ]; then rm $monkey_island_dir/nave/sabotagg.sh > /dev/null
+if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/idolo_del_governatore ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/idolo_del_governatore > /dev/null
 fi 
 
-if [ -e $monkey_island_dir/nave/suicida.sh ]; then rm $monkey_island_dir/nave/suicida.sh > /dev/null
+if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/boccale_di_grog ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/boccale_di_grog > /dev/null
 fi
 
-if [ -e $monkey_island_dir/nave/furtiva.sh ]; then rm $monkey_island_dir/nave/furtiva.sh > /dev/null
+if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/mentine_per_alito ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/mentine_per_alito > /dev/null
 fi
 
-cp $monkey_island_dir/nave/.altre/seppia.sh $monkey_island_dir/nave/ #> /dev/null
-cp $monkey_island_dir/nave/.altre/diversiv.sh  $monkey_island_dir/nave/ #> /dev/null
-cp $monkey_island_dir/nave/.altre/sabotagg.sh $monkey_island_dir/nave/ #> /dev/null
-cp $monkey_island_dir/nave/.altre/suicida.sh $monkey_island_dir/nave/ #> /dev/null
-cp $monkey_island_dir/nave/.altre/furtiva.sh    $monkey_island_dir/nave/ #> /dev/null
-#chmod +x $monkey_island_dir/nave*
 clear
 cat $monkey_island_dir/.settings/labir.txt
 
-cd $monkey_island_dir/nave
+cd $monkey_island_dir/equipaggiamento
---
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione_old.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -e $monkey_island_dir/nave/seppia.sh ]; then rm $monkey_island_dir/nave/seppia.sh > /dev/null 
+fi
+
+if [ -e $monkey_island_dir/nave/diversiv.sh ]; then rm $monkey_island_dir/nave/diversiv.sh > /dev/null 
+fi
+
+if [ -e $monkey_island_dir/nave/sabotagg.sh ]; then rm $monkey_island_dir/nave/sabotagg.sh > /dev/null
+fi 
+
+if [ -e $monkey_island_dir/nave/suicida.sh ]; then rm $monkey_island_dir/nave/suicida.sh > /dev/null
+fi
+
+if [ -e $monkey_island_dir/nave/furtiva.sh ]; then rm $monkey_island_dir/nave/furtiva.sh > /dev/null
+fi
+
+cp $monkey_island_dir/nave/.altre/seppia.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/diversiv.sh  $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/sabotagg.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/suicida.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/furtiva.sh    $monkey_island_dir/nave/ #> /dev/null
+#chmod +x $monkey_island_dir/nave*
+clear
+cat $monkey_island_dir/.settings/labir.txt
+
+cd $monkey_island_dir/nave
