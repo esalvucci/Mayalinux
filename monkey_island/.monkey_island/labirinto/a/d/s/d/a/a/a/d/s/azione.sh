a75088af1c524d4fa0e9d879012ff71985193a06
Enrico Salvucci <esalvuz@hotmail.it>
2012-05-01 16:41:49 +0200

    cambiata matrixdir in monkey_island_dir nel file azione.sh dentro il labirinto

--- monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if [ -e $matrixdir/nave/seppia.sh ]; then rm $matrixdir/nave/seppia.sh > /dev/null 
+if [ -e $monkey_island_dir/nave/seppia.sh ]; then rm $monkey_island_dir/nave/seppia.sh > /dev/null 
 fi
 
-if [ -e $matrixdir/nave/diversiv.sh ]; then rm $matrixdir/nave/diversiv.sh > /dev/null 
+if [ -e $monkey_island_dir/nave/diversiv.sh ]; then rm $monkey_island_dir/nave/diversiv.sh > /dev/null 
 fi
 
-if [ -e $matrixdir/nave/sabotagg.sh ]; then rm $matrixdir/nave/sabotagg.sh > /dev/null
+if [ -e $monkey_island_dir/nave/sabotagg.sh ]; then rm $monkey_island_dir/nave/sabotagg.sh > /dev/null
 fi 
 
-if [ -e $matrixdir/nave/suicida.sh ]; then rm $matrixdir/nave/suicida.sh > /dev/null
+if [ -e $monkey_island_dir/nave/suicida.sh ]; then rm $monkey_island_dir/nave/suicida.sh > /dev/null
 fi
 
-if [ -e $matrixdir/nave/furtiva.sh ]; then rm $matrixdir/nave/furtiva.sh > /dev/null
+if [ -e $monkey_island_dir/nave/furtiva.sh ]; then rm $monkey_island_dir/nave/furtiva.sh > /dev/null
 fi
 
-cp $matrixdir/nave/.altre/seppia.sh $matrixdir/nave/ #> /dev/null
-cp $matrixdir/nave/.altre/diversiv.sh  $matrixdir/nave/ #> /dev/null
-cp $matrixdir/nave/.altre/sabotagg.sh $matrixdir/nave/ #> /dev/null
-cp $matrixdir/nave/.altre/suicida.sh $matrixdir/nave/ #> /dev/null
-cp $matrixdir/nave/.altre/furtiva.sh    $matrixdir/nave/ #> /dev/null
-chmod +x $matrixdir/nave*
+cp $monkey_island_dir/nave/.altre/seppia.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/diversiv.sh  $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/sabotagg.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/suicida.sh $monkey_island_dir/nave/ #> /dev/null
+cp $monkey_island_dir/nave/.altre/furtiva.sh    $monkey_island_dir/nave/ #> /dev/null
+chmod +x $monkey_island_dir/nave*
 clear
-cat $matrixdir/.settings/labir.txt
+cat $monkey_island_dir/.settings/labir.txt
 
-cd $matrixdir/nave
+cd $monkey_island_dir/nave
