7580023a2da1316d40297b76649e9bc595832b75
Enrico Salvucci <esalvuz@hotmail.it>
2012-05-01 17:02:11 +0200

    senza commento

--- monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
@@ -20,7 +20,7 @@ cp $monkey_island_dir/nave/.altre/diversiv.sh  $monkey_island_dir/nave/ #> /dev/
 cp $monkey_island_dir/nave/.altre/sabotagg.sh $monkey_island_dir/nave/ #> /dev/null
 cp $monkey_island_dir/nave/.altre/suicida.sh $monkey_island_dir/nave/ #> /dev/null
 cp $monkey_island_dir/nave/.altre/furtiva.sh    $monkey_island_dir/nave/ #> /dev/null
-chmod +x $monkey_island_dir/nave*
+#chmod +x $monkey_island_dir/nave*
 clear
 cat $monkey_island_dir/.settings/labir.txt
 
