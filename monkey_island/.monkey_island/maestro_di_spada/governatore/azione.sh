c371194ce80718e67098b13aa64c9f9d0d8ea8a7
Enrico <aironetrasognato@gmail.com>
2012-07-02 16:00:28 +0200

    aggiornato azione, il codice è banana

--- monkey_island/.monkey_island/maestro_di_spada/governatore/azione.sh
+++ monkey_island/.monkey_island/maestro_di_spada/governatore/azione.sh
@@ -8,25 +8,31 @@ echo COMPLIMENTI!
 echo
 echo Hai risposto saggiamente!
 echo 
-echo Il governatore ti libera ma, mentre.
-echo Prova a cercarla nei computer del forte, dovrebbe esserci un archivio dei prigionieri!
+echo Il governatore ti libera ma, uscito dal palazzo, scopri che lo sceriffo Fester ha rapito il governatore.
+echo 
+echo Ti serve un equipaggio ed una nave per salvare Elaine Marley!
 echo
+echo Una nave costa molto, per questo dovrai chiedere una nota di credito al negoziante del villaggio, il quale però, rifiuterà il prestito.
+echo ...se solo riuscissi ad aprire la cassaforte del negoziante... 
+echo 
+echo
+echo Dovrai aprire la cassaforte in assenza del vecchietto, sopri il codice criptato, cerca nell\'archivio del negozio con il comando "ls -l", facendo attenzione ad ore e minuti!
+echo
+echo Quando avrai scoperto il codice rinomina il file banana con il comando "mv banana <codice>" ed esegui "source azione.sh" 
 rmdir uomo
 
-if [ ! -e ../../computer/cella ]; then
-touch ../../computer/cella
+if [ ! -e ../../negozio/banana ]; then
+touch ../../negozio/banana
 fi
 
-cd ../../computer
+cd ../../negozio
 
-touch -t 201108230604 $matrixdir/computer/archivio/agente
-touch -t 201108230402 $matrixdir/computer/archivio/eletto
-touch -t 201108230703 $matrixdir/computer/archivio/matrice
-touch -t 201108230503 $matrixdir/computer/archivio/oracolo
-touch -t 201108230105 $matrixdir/computer/archivio/seppia
-touch -t 201108230204 $matrixdir/computer/archivio/sorgente
-touch -t 201108230001 $matrixdir/computer/archivio/vettore
-touch -t 201108230302 $matrixdir/computer/archivio/zion
+touch -t 201108230404 $mayalinuxdir/negozio/archivio/backup
+touch -t 201108230202 $mayalinuxdir/negozio/archivio/governatore
+touch -t 201108230103 $mayalinuxdir/negozio/archivio/lampone
+touch -t 201108230301 $mayalinuxdir/negozio/archivio/nave
+touch -t 201108230502 $mayalinuxdir/negozio/archivio/Fester
+touch -t 201108230605 $mayalinuxdir/negozio/archivio/Jolly_Roger
 }
 
 sbagliato()
