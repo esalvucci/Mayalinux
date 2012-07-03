3e43e3aad890fbb298fea5c48797014e71dd24bf
Enrico <aironetrasognato@gmail.com>
2012-07-03 18:29:49 +0200

    azione.sh

--- monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
@@ -3,4 +3,8 @@
 clear
 cat $monkey_island_dir/.settings/labir.txt
 
+if [ !-d $monkey_island_dir/equipaggiamento/equipaggiamento ]; then
+mkdir $monkey_island_dir/equipaggiamento/equipaggiamento
+fi
+
 cd $monkey_island_dir/equipaggiamento
