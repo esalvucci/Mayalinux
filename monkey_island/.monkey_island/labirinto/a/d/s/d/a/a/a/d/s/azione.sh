3b25e769e62c0fb5042fd7d8c3cc162f58168436
Enrico <aironetrasognato@gmail.com>
2012-07-03 16:16:11 +0200

    finalmente sono riuscito a sistemare il labirinto

--- monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
+++ monkey_island/.monkey_island/labirinto/a/d/s/d/a/a/a/d/s/azione.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/pollo ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/pollo > /dev/null 
+if [ -e $matrixdir/nave/seppia.sh ]; then rm $matrixdir/nave/seppia.sh > /dev/null 
 fi
 
-if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/vanga ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/vanga > /dev/null 
+if [ -e $matrixdir/nave/diversiv.sh ]; then rm $matrixdir/nave/diversiv.sh > /dev/null 
 fi
 
-if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/idolo_del_governatore ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/idolo_del_governatore > /dev/null
+if [ -e $matrixdir/nave/sabotagg.sh ]; then rm $matrixdir/nave/sabotagg.sh > /dev/null
 fi 
 
-if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/boccale_di_grog ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/boccale_di_grog > /dev/null
+if [ -e $matrixdir/nave/suicida.sh ]; then rm $matrixdir/nave/suicida.sh > /dev/null
 fi
 
-if [ -e $monkey_island_dir/equipaggiamento/equipaggiamento/mentine_per_alito ]; then rm $monkey_island_dir/equipaggiamento/equipaggiamento/mentine_per_alito > /dev/null
+if [ -e $matrixdir/nave/furtiva.sh ]; then rm $matrixdir/nave/furtiva.sh > /dev/null
 fi
 
+cp $matrixdir/nave/.altre/seppia.sh $matrixdir/nave/ #> /dev/null
+cp $matrixdir/nave/.altre/diversiv.sh  $matrixdir/nave/ #> /dev/null
+cp $matrixdir/nave/.altre/sabotagg.sh $matrixdir/nave/ #> /dev/null
+cp $matrixdir/nave/.altre/suicida.sh $matrixdir/nave/ #> /dev/null
+cp $matrixdir/nave/.altre/furtiva.sh    $matrixdir/nave/ #> /dev/null
+chmod +x $matrixdir/nave*
 clear
-cat $monkey_island_dir/.settings/labir.txt
+cat $matrixdir/.settings/labir.txt
 
-cd $monkey_island_dir/equipaggiamento
+cd $matrixdir/nave
