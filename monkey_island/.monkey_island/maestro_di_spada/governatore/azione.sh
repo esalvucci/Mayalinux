5e5885eabaa16b6b42a1a79ca113588bda31bbc7
Enrico <aironetrasognato@gmail.com>
2012-07-03 10:19:36 +0200

    aggiornato livello cella -> vigilant in banana -> monkey

--- monkey_island/.monkey_island/maestro_di_spada/governatore/azione.sh
+++ monkey_island/.monkey_island/maestro_di_spada/governatore/azione.sh
@@ -12,13 +12,7 @@ echo Il governatore ti libera ma, uscito dal palazzo, scopri che lo sceriffo Fes
 echo 
 echo Ti serve un equipaggio ed una nave per salvare Elaine Marley!
 echo
-echo Una nave costa molto, per questo dovrai chiedere una nota di credito al negoziante del villaggio, il quale però, rifiuterà il prestito.
-echo ...se solo riuscissi ad aprire la cassaforte del negoziante... 
-echo 
-echo
-echo Dovrai aprire la cassaforte in assenza del vecchietto, sopri il codice criptato, cerca nell\'archivio del negozio con il comando "ls -l", facendo attenzione ad ore e minuti!
-echo
-echo Quando avrai scoperto il codice rinomina il file banana con il comando "mv banana <codice>" ed esegui "source azione.sh" 
+ 
 rmdir uomo
 
 if [ ! -e ../../negozio/banana ]; then
@@ -27,12 +21,12 @@ fi
 
 cd ../../negozio
 
-touch -t 201108230404 $mayalinuxdir/negozio/archivio/backup
-touch -t 201108230202 $mayalinuxdir/negozio/archivio/governatore
-touch -t 201108230103 $mayalinuxdir/negozio/archivio/lampone
-touch -t 201108230301 $mayalinuxdir/negozio/archivio/nave
-touch -t 201108230502 $mayalinuxdir/negozio/archivio/Fester
-touch -t 201108230605 $mayalinuxdir/negozio/archivio/Jolly_Roger
+touch -t 201208230404 $monkey_island_dir/negozio/archivio/backup
+touch -t 201208230202 $monkey_island_dir/negozio/archivio/governatore
+touch -t 201208230103 $monkey_island_dir/negozio/archivio/lampone
+touch -t 201208230301 $monkey_island_dir/negozio/archivio/nave
+touch -t 201208230502 $monkey_island_dir/negozio/archivio/Fester
+touch -t 201208230605 $monkey_island_dir/negozio/archivio/Jolly_Roger
 }
 
 sbagliato()
