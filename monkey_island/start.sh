bbd3d284b8a27e71da8a65b08cc0f9fcd19b303e
Enrico <aironetrasognato@gmail.com>
2012-09-10 18:38:36 +0200

    sono stati commentati i passaggi di start.sh in monkey_island

--- monkey_island/start.sh
+++ monkey_island/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Il path HOME punta a "labirinto" in modo che i ragazzi non tornino nella home con il comando cd
 export HOME='/home/'$USER'/Documenti/mayalinux/monkey_island/monkey_island/labirinto/'
+# vengono definiti i colori e il titolo del terminale
 export PS1='\e[1;35m \[\e]0;Monkey Island\a\]Monkey Island $ '
 
-
+# viene rimossa la cartella monkey_island (e poi ricreata) in modo che le modifiche ai file, fatte durante una partita, vengano eliminate prima della partita sucessiva
 rm -rf ./monkey_island
 
 cp -r ./.monkey_island/ ./monkey_island/
