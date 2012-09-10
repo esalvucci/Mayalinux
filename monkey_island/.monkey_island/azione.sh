9d3ba484135910e1337142cbfe68b07869967dd0
Enrico <aironetrasognato@gmail.com>
2012-09-10 18:45:04 +0200

    sono stati commentati i passaggi di azione.sh in [.monkey_island]

--- monkey_island/.monkey_island/azione.sh
+++ monkey_island/.monkey_island/azione.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 export monkey_island_dir=`pwd`
+
+# per la versione 2013 è stato ripristinato il trucco che porta alla fine del labirinto
+# digitare l'alias "lab", la password richiesta è "lab"
 alias lab="cd '$monkey_island_dir'/.settings && source trucco_labirinto.sh"
 
+# inizia il lancio del gioco, con titolo, autori e spiegazione
 cat .settings/titolo.txt
 echo Premi Invio...
 read
@@ -20,13 +24,20 @@ read
 clear
 cat .settings/leggimi.txt
 echo Premi Invio per entrare nella boscaglia...
-#play .settings/Monkey_Island_Theme.mp3 &> /dev/null 
-
-#mpg123 .settings/Monkey_Island_Theme.mp3 >& /dev/null &  
-
 read
+
+#
+#play .settings/Monkey_Island_Theme.mp3 &> /dev/null 
+#
+#mpg123 .settings/Monkey_Island_Theme.mp3 >& /dev/null &  
+#
+#play ../.settings/Monkey_Island_Theme.mp3 -q >& /dev/null &
+#
+
+# Nel 2012 abbiamo provato ad usare il comando play/mpg123 per fare sentire la musica di Monkey Island ma c'era il problema dell'output alla fine della canzone (e non all'inizio)
+
+
 
 
 cd $monkey_island_dir/labirinto
-#play ../.settings/Monkey_Island_Theme.mp3 -q >& /dev/null &
 
