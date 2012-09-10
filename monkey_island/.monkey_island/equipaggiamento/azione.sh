9038454ef6f497ff0746f3590cf2fea7698b7f2d
Enrico <aironetrasognato@gmail.com>
2012-09-10 18:49:22 +0200

    sono stati commentati i passaggi di azione.sh in [.monkey_island/equipaggiamento]

--- monkey_island/.monkey_island/equipaggiamento/azione.sh
+++ monkey_island/.monkey_island/equipaggiamento/azione.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#set livello 3
+
+#
+# ci sarebbe da impostare il controllo dei livelli
+# set livello 3
+#
+
+#se la cartella equipaggiamento è vuota evidentemente non ci si è equipaggiati!
 
 if [ ! -e ../equipaggiamento/equipaggiamento/mentine_per_alito ] && [ ! -e ../equipaggiamento/equipaggiamento/boccale_di_grog ] && [ ! -e ../equipaggiamento/equipaggiamento/idolo_del_governatore ] && [ ! -e ../equipaggiamento/equipaggiamento/pollo_di_gomma ] && [ ! -e ../equipaggiamento/equipaggiamento/vanga ]; then 
 
@@ -10,7 +16,11 @@ if [ ! -e ../equipaggiamento/equipaggiamento/mentine_per_alito ] && [ ! -e ../eq
 	cd ../equipaggiamento
 
 else
-
+#
+#per la versione 2013 si potrebbe pensare di mettere l'allenamento contro i pirati con gli insulti
+# pro: sarebbe molto bello
+# contro: rende il gioco molto più lungo, e non c'è tempo, siamo in ritardo!!!
+#
 
 echo 
 echo Dopo esserti equipaggiato con l\'oggetto che pensi ti sia utile sei pronto per allenarti contro gli altri pirati...
@@ -29,7 +39,6 @@ echo
 echo
 echo
 sleep 1
-#cd ../maestro_di_spada
 source ../maestro_di_spada/azione.sh
 
 fi
