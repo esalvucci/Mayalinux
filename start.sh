e3fcf69d05af95397b662c3fbbc9b4d9d790e831
Enrico Salvucci <enrico.salvucci@studio.unibo.it>
2015-06-27 21:35:36 +0200

    Corretto nome e colore del prompt in base a tema.

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 # permetto di eseguire tutti i file, compresi quelli nelle cartelle e sottocartelle
 chmod -Rf +x * 
 
+AMBIENTAZIONI=(matrix monkey_island)
+
 # Le versioni di Mayalinux sono 2: Matrix o Monkey Island, scegliamo a quale vogliamo giocare
 echo "Quale avventura vuoi iniziare?"
-echo "        Matrix        /        Monkey Island        "
+echo $AMBIENTAZIONI
 echo
 echo " 1 - Matrix"
 echo " 2 - Monkey Island"
@@ -23,24 +25,31 @@ else
         elif [ $risposta -eq 2 ] ; then
                 GAME="monkey_island"
         fi
+
+	# Ora che so il gioco scelto posso utilizzare $GAME
+
+	# Il path HOME punta a "labirinto" in modo che i ragazzi non tornino nella home con il comando cd
+	export HOME=$PWD/$GAME'/livello_0/'
+
+	# GAME_TITLE --> $GAME senza "_" e con l' iniziale maiuscola
+	GAME_TITLE=$(echo "$GAME" | sed -r 's/(^)([a-z])/\U\2/g' | sed -r 's/(_)([a-z])/ \2/g')
+
+	# vengono definiti i colori del terminale in base all'ambientazione del gioco
+	if [ $GAME == 'matrix' ] ; then
+ 		export PS1='\[\e[0;32m\]'$GAME_TITLE' $\[\e[m\] \[\e[0;37m\] '
+	fi
+	
+	if [ $GAME == 'monkey_island' ] ; then
+ 		export PS1='\[\e[1;35m\]'$GAME_TITLE' $\[\e[m\] \[\e[0;37m\] '
+	fi
+
+	# viene rimossa la cartella monkey_island (e poi ricreata) in modo che le modifiche ai file, fatte durante una partita, vengano eliminate prima della partita sucessiva                         
+	if [ -d $GAME ] ; then
+		rm -rf $GAME                                                                                                                                                                                    
 fi
 
-# Ora che so il gioco scelto posso utilizzare $GAME
+	cp -ar .game $GAME
+	cd $GAME
+	source azione.sh
 
-# Il path HOME punta a "labirinto" in modo che i ragazzi non tornino nella home con il comando cd
-export HOME=$PWD/$GAME'/livello_0/'
-
-## TODO
-# GAME_TITLE --> $GAME senza "_" e con le iniziali maiuscole
-GAME_TITLE=$GAME        # +++ Da correggere +++
-
-# vengono definiti i colori e il titolo del terminale
-export PS1='\e[1;35m \[\e]0;'$GAME_TITLE'\a\]'$GAME_TITLE' $ '                                                                                                                                  
-# viene rimossa la cartella monkey_island (e poi ricreata) in modo che le modifiche ai file, fatte durante una partita, vengano eliminate prima della partita sucessiva                         
-if [ -d $GAME ] ; then
-	rm -rf $GAME                                                                                                                                                                                    
 fi
-
-cp -ar .game $GAME
-cd $GAME
-source azione.sh
